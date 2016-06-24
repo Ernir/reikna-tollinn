0e6f886c93ff3be7f9ef54a8d3f753a07d5bda0c
Ernir <e.ernir@gmail.com>
2016-06-24 18:21:47 +0000

    Apply the updated customs rules

--- js/app.js
+++ js/app.js
@@ -1,26 +1,55 @@
 "use strict";
 
-function calculateRemaining(n, spiritAmount, wineAmount, beerAmount) {
+function litresToUnits(spiritAmount, wineAmount, beerAmount) {
+    /*
+     Converts given amounts of alcohol to "units" defined by Icelandic customs.
+
+     spiritAmount: litres of hard liquor
+     wineAmount: litres of wine
+     beerAmount: litres of beer
+     */
+
+    // Units defined by https://www.tollur.is/einstaklingar/tollamal/ferdamenn/
     var spiritUnit = 0.25;
     var wineUnit = 0.75;
-    
+    var beerUnit = 3;
+
+    var unitsFromSpirits = Math.ceil(spiritAmount / spiritUnit);
+    var unitsFromWine = Math.ceil(wineAmount / wineUnit);
+    var unitsFromBeer = Math.ceil(beerAmount / beerUnit);
+
+    return unitsFromSpirits + unitsFromWine + unitsFromBeer;
+
+}
+
+function calculateRemaining(numPeople, units) {
+    /*
+     Calculates the number of tax-free units a group has remaining.
+
+     numPeople: the number of people travelling together
+     units: the combined number of units of alcohol the group is carrying
+     */
+
+    // Limit defined by https://www.tollur.is/einstaklingar/tollamal/ferdamenn/
+    var unitsPerPerson = 6;
+    return numPeople * unitsPerPerson - units;
 }
 
 
-function updateMessages(remainders) {
-
+function updateMessages(remainder) {
 }
 
 function applyListeners() {
     $("form").change(function () {
 
-        var n = parseInt($("#num-people").val());
+        var numPeople = parseInt($("#num-people").val());
         var spirits = parseFloat($("#spirit-amount").val());
         var wine = parseFloat($("#wine-amount").val());
         var beer = parseFloat($("#beer-amount").val());
 
-        var remainders = calculateRemaining(n, spirits, wine, beer);
-        updateMessages(remainders);
+        var units = litresToUnits(spirits, wine, beer);
+        var remaining = calculateRemaining(numPeople, units);
+        updateMessages(remaining);
     })
 }
 
